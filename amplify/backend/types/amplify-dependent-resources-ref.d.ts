ac370b9b1f9dd58acf4dcd306221fc77b65381be
colin mcgovern <colinmcg@udel.edu>
2023-05-01 12:51:33 -0400

    first

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,41 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "petsapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "petsapi0c68bdb8": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "petsfunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "petsDB": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import config from './aws-exports'
+import { Amplify, API } from 'aws-amplify'
+import {Authenticator } from '@aws-amplify/ui-react'
+import './App.css'
+
+Amplify.configure(config)
 
 function App() {
+  
+  const [petName, setPetName] = React.useState('')
+  const [petType, setPetType] = React.useState('')
+  const [pets, setPets] = React.useState([])
+
+  useEffect(() => {
+    API.get('petsapi','/pets/name')
+    .then(petRes=>setPets([...pets,...petRes]))
+  }, [])
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    API.post('petsapi','/pets',{
+      body: {
+        name: petName,
+        type: petType,
+      },
+    }).then(fetchedPets => {
+      setPets([...pets,{name: petName, type: petType}])
+    })
+    return true
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+            <form onSubmit={handleSubmit}>
+              <input value={petName} placeholder="name_here" onChange={(e) => setPetName(e.target.value)}/>
+              <input value={petType} placeholder="type_here" onChange={(e) => setPetType(e.target.value)}/>
+              <button>Add Pet</button>
+            </form>
+
+            <ul>
+              {pets.map(pet => <li>{pet.name}</li>)}
+            </ul>
+
+
+{/*          <Authenticator>
+            {({ signOut, user }) => (
+              <div className="App">
+                <p>
+                  Hey {user.username}, welcome to my channel, with auth!
+                </p>
+                <button onClick={signOut}>Sign out</button>
+              </div>
+            )}
+          </Authenticator>*/}
       </header>
     </div>
   );
